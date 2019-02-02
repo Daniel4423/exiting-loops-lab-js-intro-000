6eddde175b617eac3d84a5d268ebc05fd22f0404
Daniel Bailey <daniel4423@gmail.com>
2019-02-02 02:06:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-  if findFn() === true {
+  if (findFn() === true) {
   for (var i = 0; i <= array.length; i++) {
     if (array[i] === found) {
       return findFn()
